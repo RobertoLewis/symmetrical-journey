c456b52f99739d749c5a69a983c1f7ff6b955e67
Roberto Lewis <roberto.lewis@gmail.com>
2017-01-09 16:07:09 -0800

    [progress] added front-end parse logic

--- wuIpsum/client/js/home.js
+++ wuIpsum/client/js/home.js
@@ -1,25 +1,39 @@
+
+
 $(document).ready(function() {
+
+  // Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+  console.log("success!!");
+} else {
+  console.log('The File APIs are not fully supported in this browser.');
+}
+
   $(".pullText").click(function(){
     console.log("log it all!!");
+    $.get('"../sonnetCXXX.txt"', function(data) {
+    console.log(data);
+});
 
-    // $.get("localhost:3000/lyrics", function(data){ console.log(data) },
-    //   "text/html"
+    // var reader = new FileReader();
+    // var lyrics = '';
+    // var newFile = new File([""], "../sonnetCXXX.txt");
     //
-    // );
+    // reader.onload = function(e) {
+    //   lyrics = reader.result;
+    // };
+    //
+    // var output = new Promise (function(resolve, reject){
+    //
+    //   reader.readAsText(newFile);
+    //   resolve(console.log(lyrics, "yay"));
+    // });
 
-    // I'm getting errors below
-    var jqxhr = $.get( "localhost:3000/lyrics", function() {
-  console.log( "success" );
-})
-  .done(function() {
-    console.log( "second success" );
-  })
-  .fail(function() {
-    console.log( "error" );
-  })
-  .always(function() {
-    console.log( "finished" );
-  });
+    // lyricFile = Parse.getVerses(2, lyrics);
+    // console.log(lyricFile);
+
+
 
   });
 
