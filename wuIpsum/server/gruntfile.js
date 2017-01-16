f4446fdc2f12c3f36d5de660d74b5e2a5171e384
Roberto Lewis <roberto.lewis@gmail.com>
2017-01-16 14:15:01 -0800

    [add] adds gruntfile

---
+++ wuIpsum/server/gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat']);
+
+};
