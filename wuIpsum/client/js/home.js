f2e11269d5777a6b7c5193dea35520e09539270a
Roberto Lewis <roberto.lewis@gmail.com>
2017-01-15 23:03:19 -0800

    [add] add banner image

--- wuIpsum/client/js/home.js
+++ wuIpsum/client/js/home.js
@@ -2,38 +2,12 @@
 
 $(document).ready(function() {
 
-  // Check for the various File API support.
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  // Great success! All the File APIs are supported.
-  console.log("success!!");
-} else {
-  console.log('The File APIs are not fully supported in this browser.');
-}
-
   $(".pullText").click(function(){
     console.log("log it all!!");
-    $.get('"../sonnetCXXX.txt"', function(data) {
-    console.log(data);
+    $.get("/lyrics", function(data) {
+    // console.log(data);
+    $(".contentText").append(data);
 });
-
-    // var reader = new FileReader();
-    // var lyrics = '';
-    // var newFile = new File([""], "../sonnetCXXX.txt");
-    //
-    // reader.onload = function(e) {
-    //   lyrics = reader.result;
-    // };
-    //
-    // var output = new Promise (function(resolve, reject){
-    //
-    //   reader.readAsText(newFile);
-    //   resolve(console.log(lyrics, "yay"));
-    // });
-
-    // lyricFile = Parse.getVerses(2, lyrics);
-    // console.log(lyricFile);
-
-
 
   });
 
